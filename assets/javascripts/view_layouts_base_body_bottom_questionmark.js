6527673cc60cd71b3383f8daa49cd142f2b7580d
Wake Liu <wake.gs@gmail.com>
2020-11-04 11:57:37 +0800

    Enable `question-mark-modal` only after user signed-in

--- assets/javascripts/view_layouts_base_body_bottom_questionmark.js
+++ assets/javascripts/view_layouts_base_body_bottom_questionmark.js
@@ -3,34 +3,39 @@ $questionMarkModalRoot = $('<div class="question-mark-modal-root"></div>');
 $questionMarkModal = $('<div class="question-mark-modal"><span aria-label="Close" aria-role="button" class="question-mark--close"/></div>')
 $questionMarkModalRoot.append($questionMarkModal);
 $questionMarkModal.append('<iframe width="500px" height="500px" src="' + window.location.origin + '/redmine_shortcuts/help"></iframe>');
-$('body').append($questionMarkModalRoot);
-$(document).on('keypress', function(e) {
-  e = e || window.event;
-  var charCode = e.keyCode || e.which;
-  // 63 = '?' key
-  if (charCode === 63 && !$(':focus').is('textarea, input')) {
-    $questionMarkModalRoot.toggleClass('question-mark-modal-visible');
-  }
-});
-$(document).on('keyup', function(e) {
-  e = e || window.event;
-  var charCode = e.keyCode || e.which;
-  // 27 = 'Esc' key
-  if (charCode === 27) {
+
+if ($('a[href="/logout"]').length >= 1) {
+
+  $('body').append($questionMarkModalRoot);
+
+  $(document).on('keypress', function(e) {
+    e = e || window.event;
+    var charCode = e.keyCode || e.which;
+    // 63 = '?' key
+    if (charCode === 63 && !$(':focus').is('textarea, input')) {
+      $questionMarkModalRoot.toggleClass('question-mark-modal-visible');
+    }
+  });
+  $(document).on('keyup', function(e) {
+    e = e || window.event;
+    var charCode = e.keyCode || e.which;
+    // 27 = 'Esc' key
+    if (charCode === 27) {
+      $questionMarkModalRoot.removeClass('question-mark-modal-visible');
+    }
+  });
+  $('.question-mark--close').click(function() {
     $questionMarkModalRoot.removeClass('question-mark-modal-visible');
-  }
-});
-$('.question-mark--close').click(function() {
-  $questionMarkModalRoot.removeClass('question-mark-modal-visible');
-});
+  });
 
-$questionMarkModalRoot.click(function() {
-  $(this).removeClass('question-mark-modal-visible');
-});
+  $questionMarkModalRoot.click(function() {
+    $(this).removeClass('question-mark-modal-visible');
+  });
 
-$questionMarkModal.click(function(event) {
-  event.preventDefault();
-  event.stopPropagation();
-  event.stopImmediatePropagation();
-  return false;
-});
+  $questionMarkModal.click(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    event.stopImmediatePropagation();
+    return false;
+  });
+}
